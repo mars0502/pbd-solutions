89ed25f107e4eed2c44a83b4f226656d18cb607f
mars0502 <36413750+mars0502@users.noreply.github.com>
2018-02-23 11:04:22 -0500

    Add files via upload

---
+++ 15/TheForgetfulMachine.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TheForgetfulMachine {
+     public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       
+       System.out.print("What colour is the sky? "); 
+       input.next();
+       
+       System.out.print("What is your favourite colour? ");
+       input.next();
+       
+       System.out.print("How many fingers do u have? ");
+       input.next();
+       
+       System.out.print("What is 4 x 6? ");
+       input.next();
+       }
+}
